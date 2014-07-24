fa4d5a5c420edd2cdb1916f2acf778c12f3e5f14
Jakub Jarosz <jakub.s.jarosz@gmail.com>
2014-07-24 16:13:49 +0100

    Bumping version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(os.path.join(this_dir, 'README.rst')) as f:
 
 setup(
     name='circleclient',
-    version='0.1.2',
+    version='0.1.3',
     author='Jakub Jarosz',
     author_email='qba73@postpro.net',
     description='Python client for CircleCI API',
