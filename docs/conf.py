9b3daa8cf33d8e99caf45274a1e7832a145d1417
Jakub Jarosz <qba73@postpro.net>
2015-04-09 08:18:32 +0100

    Update year to 2015

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'circleclient'
-copyright = u'2014, Jakub Jarosz'
+copyright = u'2015, Jakub Jarosz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
